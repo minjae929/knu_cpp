b00f0f9d720aaee32daa883dc3af428755bafe84
minjae929 <mj9530k@daum.net>
2025-03-13 16:07:25 +0900

    Create 5.cpp
    2-5

---
+++ chapter2/5.cpp
@@ -0,0 +1,14 @@
+//2-5//
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "문자들을 입력하라(100개 미만).>>";
+	char letter[100];
+	cin.getline(letter, 100, '\n');
+	int i, sum = 0;
+	for (i = 1; i < 100; i++) {
+		if (letter[i] == 'x') sum += 1;
+	}
+	cout << "x의 개수는 " << sum;
+}
