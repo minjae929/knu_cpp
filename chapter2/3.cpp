766008b0da650d2fb0834c237a6dcda94194c627
minjae929 <mj9530k@daum.net>
2025-03-13 15:51:07 +0900

    Create 3.cpp
    2-3

---
+++ chapter2/3.cpp
@@ -0,0 +1,11 @@
+//2-3//
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cout << "두 정수를 입력하세요 >>"; 
+	cin >> a >> b; 
+	if (a > b) cout << "큰 수 = " << a;
+	else if (a < b) cout << "큰 수 = " << b;
+}
