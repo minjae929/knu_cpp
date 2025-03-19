f438155959422eae7de8b886104947d010646123
minjae929 <mj9530k@daum.net>
2025-03-19 23:46:37 +0900

    6.cpp

--- chapter2/6.cpp
+++ chapter2/6.cpp
@@ -1,18 +1,17 @@
-//2-6//
 #include <iostream>
 #include <string>
 using namespace std;
 
 int main() {
-	string elvis;
-	string singer;
-	cout <<  "새 암호를 입력하세요 "; 
-	getline(cin, elvis); 
+	string password;
+	string againword;
+	cout <<  "새 암호를 입력하세요>>"; 
+	getline(cin, password); 
 
-	cout << "새 암호를 다시 한번 입력하세요 ";  
+	cout << "새 암호를 다시 한번 입력하세요>>";  
 
-	getline(cin, singer); 
-	if (singer == elvis)  
+	getline(cin, againword); 
+	if (againword == password)  
 		cout << "같습니다.";
 	else
 		cout << "같지 않습니다. ";
