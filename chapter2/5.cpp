98c25c968d8714243c256d9ef0163c08f7c40c04
minjae929 <mj9530k@daum.net>
2025-03-19 23:46:20 +0900

    5.cpp

--- chapter2/5.cpp
+++ chapter2/5.cpp
@@ -1,12 +1,14 @@
-//2-5//
 #include <iostream>
 using namespace std;
 
 int main() {
-	cout << "문자들을 입력하라(100개 미만).>>";
+	cout << "문자들을 입력하라(100개 미만).\n";
+
 	char letter[100];
 	cin.getline(letter, 100, '\n');
+
 	int i, sum = 0;
+
 	for (i = 1; i < 100; i++) {
 		if (letter[i] == 'x') sum += 1;
 	}
