bf164102a6d17262e480e1ec50eb0420e9182cf7
minjae929 <mj9530k@daum.net>
2025-03-26 23:34:41 +0900

    6.cpp

--- chapter3/6.cpp
+++ chapter3/6.cpp
@@ -39,12 +39,12 @@ int EvenRandom::nextInRange(int low, int high) {
 
 int main() {
 	EvenRandom er;
-	cout << "-- 0에서 " << RAND_MAX << "까지의 짝수 랜덤 정수 10 개--" << endl;
+	cout << "-- 0에서 " << RAND_MAX << "까지의 랜덤 정수 10 개--" << endl;
 	for (int i = 0; i < 10; i++) {
 		int n = er.next();
 		cout << n << ' ';
 	}
-	cout << endl << endl << "-- 2에서 " << "10 까지의 짝수 랜덤 정수 10 개 --" << endl;
+	cout << endl << endl << "-- 2에서 " << "10 까지의 랜덤 정수 10 개 --" << endl;
 	for (int i = 0; i < 10; i++) {
 		int n = er.nextInRange(2, 10);
 		cout << n << ' ';
