c19ecdce3788c75b86e166bcfccd8fbcb3c5f379
minjae929 <mj9530k@daum.net>
2025-03-26 23:21:25 +0900

    10.cpp

--- chapter3/10.cpp
+++ chapter3/10.cpp
@@ -2,71 +2,59 @@
 using namespace std;
 
 class Add {
-    int a, b;
+	int a, b;
 public:
-    void setValue(int x, int y);
-    int calculate();
+	void setValue(int x, int y) { a = x, b = y; }
+	int calculate() { return a + b; }
 };
 
 class Sub {
-    int a, b;
+	int a, b;
 public:
-    void setValue(int x, int y);
-    int calculate();
+	void setValue(int x, int y) { a = x, b = y; }
+	int calculate() { return a - b; }
 };
 
 class Mul {
-    int a, b;
+	int a, b;
 public:
-    void setValue(int x, int y);
-    int calculate();
+	void setValue(int x, int y) { a = x, b = y; }
+	int calculate() { return a * b; }
 };
 
 class Div {
-    int a, b;
+	int a, b;
 public:
-    void setValue(int x, int y);
-    int calculate();
+	void setValue(int x, int y) { a = x, b = y; }
+	int calculate() { return a / b; }
 };
 
-void Add::setValue(int x, int y) { a = x; b = y; }
-int Add::calculate() { return a + b; }
 
-void Sub::setValue(int x, int y) { a = x; b = y; }
-int Sub::calculate() { return a - b; }
-
-void Mul::setValue(int x, int y) { a = x; b = y; }
-int Mul::calculate() { return a * b; }
-
-void Div::setValue(int x, int y) { a = x; b = y; }
-int Div::calculate() { return a / b; }
-
-int main() {
-    Add a;
-    Sub s;
-    Mul m;
-    Div d;
-    int p, q;
-    string way;
-
-    while (true) {
-        cout << "두 정수와 연산자를 입력하세요>>";
-        cin >> p >> q >> way;
-        if (way == "+") {
-            a.setValue(p, q);
-            cout << a.calculate() << endl;
-        }
-        else if (way == "-") {
-            s.setValue(p, q);
-            cout << s.calculate() << endl;
-        }
-        else if (way == "*") {
-            m.setValue(p, q);
-            cout << m.calculate() << endl;
-        }
-        else if (way == "/") {
-            d.setValue(p, q);
-            cout << d.calculate() << endl;
-        }
-    }
+int main(){
+	Add a;
+	Sub s;
+	Mul m;
+	Div d;
+	int p, q;
+	string way;
+	while (true) {
+		cout << "두 정수와 연산자를 입력하세요>>";
+		cin >> p >> q >> way;
+		if (way == "+") {
+			a.setValue(p, q);
+			cout << a.calculate() << endl;
+		}
+		else if (way == "-") {
+			s.setValue(p, q);
+			cout << s.calculate() << endl;
+		}
+		else if (way == "*") {
+			m.setValue(p, q);
+			cout << m.calculate() << endl;
+		}
+		else if (way == "/") {
+			d.setValue(p, q);
+			cout << d.calculate() << endl;
+		}
+	} 
 }
